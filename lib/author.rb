ca234790d6adfc452fc5e51767082590f13095e4
Rosa Carranza <rcarranza1024@yahoo.com>
2020-05-09 22:26:52 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -17,12 +17,8 @@ class Author
  def add_post (post)
    post.author = self
  end
+end
 
- def add_post_by_name(post_name)
-   poster = Post.new(post_name)
-   add_post(poster)
- end
-  end
 
   def self.post_count
     @@post_count
