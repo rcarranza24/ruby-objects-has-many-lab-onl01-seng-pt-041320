da5ebb1d0402ce89b4d1d06b403703fc94aedc54
Rosa Carranza <rcarranza1024@yahoo.com>
2020-05-09 22:35:23 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -2,7 +2,6 @@ class Author
 
   attr_accessor :name, :posts
 
-  @@post_count = 0
 
   def initialize(name)
     @name = name
@@ -16,12 +15,6 @@ class Author
  end
 
  def add_post(post)
-     @posts << post
      post.author = self
-     @@post_count += 1
-  end
-
-  def self.post_count
-    @@post_count
   end
 end
