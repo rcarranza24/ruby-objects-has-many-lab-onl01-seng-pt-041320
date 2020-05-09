f4f3779e700bc506f0796cc13a890b73473adc47
Rosa Carranza <rcarranza1024@yahoo.com>
2020-05-09 21:57:31 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,32 +1,15 @@
 class Artist
-  attr_accessor :name
+  attr_accessor :name, :songs
 
-  @@song_count = 0
 
   def initialize(name)
     @name = name
-    @songs = []
   end
 
-  def add_song(song)
-    self.songs << song
-    song.artist = self
-    @@song_count +=1
-  end
-
-  def add_song_by_name(name)
-    song = Song.new(name)
-    self.songs << song
-    song.artist = self
-    @@song_count +=1
-  end
 
   def songs
     Song.all.select do |song|
       song.artist == self
   end
-
-  def self.song_count
-    @@song_count
-  end
+ end
 end
