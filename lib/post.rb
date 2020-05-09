3ddeebc1acd4e99bfb7b9bc9876a4f781544212c
Rosa Carranza <rcarranza1024@yahoo.com>
2020-05-09 22:19:20 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,15 +1,24 @@
 class Post
 
   attr_accessor :title, :author
+
+  @@all = []
+
   def initialize(title)
     @title = title
+    @@all << self
   end
 
-  def author_name
-    if @author == nil
-      return nil
+
+  def autho_name
+    if self.author
+      self.author.name
     else
-      @author.name
+      nil
     end
+  end
+
+  def self.all
+    @@all
   end
 end
